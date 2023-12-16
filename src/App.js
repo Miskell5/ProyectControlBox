c7f92d356c75a4d6b5e5e5af67ac53ec65227cae
Miskell5 <tatansito57@gmail.com>
2023-12-16 17:20:38 -0500

    Menú principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Mundo!</h1>
-      <p>Prueba de ambiente</p>
-    
+      <Navbar expand="lg" className="menu">
+        <Container>
+          <Navbar.Brand href="#home">Brayan's Literary Threads </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="menu_libro_usuario">
+              <Nav.Link href="#home">Libros</Nav.Link>
+              
+              <NavDropdown title="Usuario" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Perfil de Usuario</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">Cerrar Sesión</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
