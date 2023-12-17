0eaf73d1195416a02e0214ec18bfd18dfd0c0e8b
Miskell5 <tatansito57@gmail.com>
2023-12-16 21:09:28 -0500

    Página principal realizada, junto con el carrusel de imagenes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Carousel from 'react-bootstrap/Carousel';
+import imagen1 from './images/imagen_carusel1.jpg';
+import imagen2 from './images/imagen_carusel2.jpeg';
+import imagen3 from './images/imagen_carusel3.jpeg';
+
 
 function App() {
   return (
@@ -14,17 +18,47 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="menu_libro_usuario">
-              <Nav.Link href="#home">Libros</Nav.Link>
-              
-              <NavDropdown title="Usuario" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Perfil de Usuario</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.2">Cerrar Sesión</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#home">Iniciar Sesión</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <main>
+        
+        <Carousel fade>
+          <Carousel.Item interval={1000}>
+            <imagen1 text="First slide" /> 
+              <img src={ imagen1 } className='d-block w-100 imagen_carrusel' alt="..."/>        
+            <Carousel.Caption>
+              <h3>Donde cada página es una nueva aventura literaria.</h3>
+              <p>En nuestra página, cada libro es una puerta a un universo único.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <imagen2 text="Second slide" />
+            <img src={ imagen2 } className='d-block w-100 imagen_carrusel' alt="..."/>    
+            <Carousel.Caption>
+              <h3>Palabras que inspiran, historias que transforman.</h3>
+              <p>Creemos en el poder de la lectura para enriquecer tu mente y alimentar tu alma.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <imagen3 text="Third slide" />
+            <img src={ imagen3 } className='d-block w-100 imagen_carrusel' alt="..."/>    
+            <Carousel.Caption>
+              <h3>Libros que crean puentes entre mundos.</h3>
+              <p>Explora la diversidad literaria y expande tus horizontes.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </main>
+
+      <footer className="bg-dark text-light mt-0.01">
+      <Container className="text-center py-3">
+        <p>&copy; 2023 Brayan's Literary Threads. Proyecto para fines demostrativos.</p>
+        <p>Brayan Steven Horta Quevedo</p>
+      </Container>
+    </footer>
     </div>
   );
 }
